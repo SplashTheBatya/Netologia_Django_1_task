0a0c627dafbf6b64216104e00c3f609cc3f65ab8
Kakolay Nimalov <47940537+SplashTheBatya@users.noreply.github.com>
2021-02-13 01:12:06 +0300

    Task 2 version 1
     uploaded file_server, pagination and landing

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
+from django.urls import path, register_converter
 
-# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
+from app.views import *
 
+
+class DateConverter:
+    regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+
+    def to_python(self, value):
+        return datetime.datetime.strptime(value, '%Y-%m-%d').date()
+
+    def to_url(self, value):
+        return value.strftime('%Y-%m-%d')
+
+
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
-                                      # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    path('', file_list, name='file_list'),
+    path('<date:date>/', file_list, name='file_list'),
+    path('file_content/<str:name>', file_content, name='file_content'),
 ]
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,48 @@
 import datetime
+import os
 
 from django.shortcuts import render
+from django.conf import settings
 
 
-def file_list(request):
-    template_name = 'index.html'
-    
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
-    }
+files = os.listdir(settings.FILES_PATH)
 
-    return render(request, template_name, context)
+
+def file_list(request, date: datetime = None):
+    if date is None:
+        context_list = []
+        for file_iter in range(len(files)):
+            stat_res = os.stat(f'{settings.FILES_PATH}\\{files[file_iter]}')
+            context_list.append({'name': files[file_iter],
+                                'ctime': datetime.datetime.fromtimestamp(stat_res.st_ctime).date(),
+                                 'mtime': datetime.datetime.fromtimestamp(stat_res.st_mtime).date()})
+    else:
+        context_list = []
+        for file_iter in range(len(files)):
+            stat_res = os.stat(f'{settings.FILES_PATH}\\{files[file_iter]}')
+            ctime = datetime.datetime.fromtimestamp(stat_res.st_ctime).date()
+            if ctime == date:
+                context_list.append({'name': files[file_iter],
+                                     'ctime': datetime.datetime.fromtimestamp(stat_res.st_ctime).date(),
+                                     'mtime': datetime.datetime.fromtimestamp(stat_res.st_mtime).date()})
+    return render(request, 'index.html', context={
+        'files': context_list,
+        'date': date
+    })
 
 
 def file_content(request, name):
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+    for search_iter in range(len(files)):
+        if files[search_iter] == name:
+            stat_res = os.stat(f'{settings.FILES_PATH}\\{files[search_iter]}')
+            content = ''
+            f = open(f'{settings.FILES_PATH}\\{files[search_iter]}')
+            for line in f:
+                content += f.readline()
+            return render(
+                request,
+                'file_content.html',
+                context={'file_name': files[search_iter], 'file_content': content}
+
     )
 
