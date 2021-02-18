147f123acab05bc436fe9332bae1a298e8e049de
Kakolay Nimalov <47940537+SplashTheBatya@users.noreply.github.com>
2021-02-18 23:58:33 +0300

    Inflation table rework 2/18/2021

--- dynamic-templates/task1/app/templatetags/filters.py
+++ dynamic-templates/task1/app/templatetags/filters.py
@@ -4,9 +4,30 @@ register = template.Library()
 
 
 @register.filter()
-def type_filter(value: str):
-    try:
-        float(value)
-    except Exception(''):
-        return 0
-    return float(value)
+def color_filter(value):
+    if value == '-':
+        return "#ffffff"
+    elif float(value) > 1000:
+        return "#ffffff"
+    elif float(value) < 0:
+        return "darkgreen"
+    elif 1 < float(value) < 2:
+        return "#ffa48f"
+    elif 2 < float(value) < 5:
+        return "#fe6f5e"
+    elif float(value) > 5:
+        return "red"
+    else:
+        return "#ffffff"
+
+
+@register.filter()
+def get_keys_filer(value: dict):
+    return list(value.keys())
+
+
+@register.filter()
+def get_value(key, dictionary):
+    return dictionary[key]
+
+
--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -17,47 +17,45 @@ def inflation_view(request):
 
     data_list = []
 
-    # Прекрасный двойной цикл :D
     for list_iter in reader:
-        for key in list_iter.keys():
-            # Костыль :)
-            if key == 'Год':
-                list_iter[key] = '<td>' + list_iter[key]
-            # Ещё костыль ;)
-            elif key == 'Суммарная':
-                pass
-            elif list_iter[key] == '-':
-                list_iter[key] = '<td>' + list_iter[key]
-            elif float(list_iter[key]) < 0:
-                list_iter[key] = '<td style="background-color: darkgreen">' + list_iter[key]
-            elif 1 < float(list_iter[key]) < 2:
-                list_iter[key] = '<td style="background-color: #ffa48f">' + list_iter[key]
-            elif 2 < float(list_iter[key]) < 5:
-                list_iter[key] = '<td style="background-color: #fe6f5e">' + list_iter[key]
-            elif float(list_iter[key]) > 5:
-                list_iter[key] = '<td style="background-color: red">' + list_iter[key]
-            # И ещё запасной костыль :))
-            else:
-                list_iter[key] = '<td>' + list_iter[key]
-        # И наконец-то 1 строчка готова !!!
+        # for key in list_iter.keys():
+        #     # Костыль :)
+        #     if key == 'Год':
+        #         list_iter[key] = '<td>' + list_iter[key]
+        #     # Ещё костыль ;)
+        #     elif key == 'Суммарная':
+        #         pass
+        #     elif list_iter[key] == '-':
+        #         list_iter[key] = '<td>' + list_iter[key]
+        #     elif float(list_iter[key]) < 0:
+        #         list_iter[key] = '<td style="background-color: darkgreen">' + list_iter[key]
+        #     elif 1 < float(list_iter[key]) < 2:
+        #         list_iter[key] = '<td style="background-color: #ffa48f">' + list_iter[key]
+        #     elif 2 < float(list_iter[key]) < 5:
+        #         list_iter[key] = '<td style="background-color: #fe6f5e">' + list_iter[key]
+        #     elif float(list_iter[key]) > 5:
+        #         list_iter[key] = '<td style="background-color: red">' + list_iter[key]
+        #     # И ещё запасной костыль :))
+        #     else:
+        #         list_iter[key] = '<td>' + list_iter[key]
+        # # И наконец-то 1 строчка готова !!!
         data_list.append({
-            'Year': list_iter['Год'],
-            'Jan': list_iter['Янв'],
-            'Feb': list_iter['Фев'],
-            'Mar': list_iter['Мар'],
-            'Apr': list_iter['Апр'],
-            'May': list_iter['Май'],
-            'Jun': list_iter['Июн'],
-            'Jul': list_iter['Июл'],
-            'Aug': list_iter['Авг'],
-            'Sep': list_iter['Сен'],
-            'Oct': list_iter['Окт'],
-            'Nov': list_iter['Ноя'],
-            'Dec': list_iter['Дек'],
-            'Summary': list_iter['Суммарная'],
+            'Год': list_iter['Год'],
+            'Янв': list_iter['Янв'],
+            'Фев': list_iter['Фев'],
+            'Мар': list_iter['Мар'],
+            'Апр': list_iter['Апр'],
+            'Май': list_iter['Май'],
+            'Июн': list_iter['Июн'],
+            'Июл': list_iter['Июл'],
+            'Авг': list_iter['Авг'],
+            'Сен': list_iter['Сен'],
+            'Окт': list_iter['Окт'],
+            'Ноя': list_iter['Ноя'],
+            'Дек': list_iter['Дек'],
+            'Суммарная': list_iter['Суммарная'],
         })
-
-    # Ура, солнце зашло, Джанго может отдохнуть
+    print(data_list)
     context = {'years_data': data_list}
 
     return render(request, template_name,
